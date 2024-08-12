b1923365b37378fe60eea5aecc9402995fc2f266
blank-hm <13951016530@163.com>
2024-08-12 21:48:50 +0800

    feat: 最后更新日期

--- themes/gitbook/components/ArticleInfo.js
+++ themes/gitbook/components/ArticleInfo.js
@@ -4,6 +4,6 @@ export default function ArticleInfo({ post }) {
   }
   return <div className="pt-10 pb-6 text-gray-400 text-sm border-b">
         <i className="fa-regular fa-clock mr-1" />
-        Last update:  { post.date?.start_date}
+        最后更新日期:  { post.date?.start_date}
     </div>
 }
